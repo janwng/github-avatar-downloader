be534428f0d5965d4222575972bc94f144e96255
Jan Wong <janwong94@gmail.com>
2017-03-14 03:17:40 +0000

    create the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+var GITHUB_USER = "janwng";
+var GITHUB_TOKEN = "65bf00c8ec473576306dd40cca18204b39b2b8a1";
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
